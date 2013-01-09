bf88f3e41a3ab11034822404a1966c9b908f314a
travp624 <travp624@gmail.com>
2013-01-09 07:31:50 -0600

    add verizon login client to verizon blobs

--- device-verizon.mk
+++ device-verizon.mk
@@ -1,10 +1,12 @@
-# Blobs necessary for verizon nexus devices
+# Vzw permissions
+PRODUCT_COPY_FILES += \
+    device/samsung/toro/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    device/samsung/toro/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
+    device/samsung/toro/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml
 
-PRODUCT_COPY_FILES := \
-    vendor/verizon/proprietary/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
-    vendor/verizon/proprietary/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
-    vendor/verizon/proprietary/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
-
+# Blobs necessary for verizon devices
 PRODUCT_PACKAGES := \
+    libmotricity \
+    VerizonSSO \
     VZWAPNLib \
     VZWAPNService
